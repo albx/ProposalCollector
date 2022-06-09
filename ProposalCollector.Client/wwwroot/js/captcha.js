f7be6f52e51c5390a5a9058038bbd1644a8c5833
Alberto Mori <albi.mo87@hotmail.it>
2022-06-09 19:12:49 +0200

    first captcha implementation

---
+++ ProposalCollector.Client/wwwroot/js/captcha.js
@@ -0,0 +1,11 @@
+﻿function initializeCaptcha(dotNetObject, selector, sitekeyValue) {
+    return grecaptcha.render(selector, {
+        'sitekey': sitekeyValue,
+        'callback': (response) => { dotNetObject.invokeMethodAsync('Success', response); },
+        'expired-callback': () => { dotNetObject.invokeMethodAsync('Expired', response); }
+    });
+}
+
+function getResponse(response) {
+    return grecaptcha.getResponse(response);
+}
